b092567204ab8d171b057f1cec7bf705ae439563
Rajesh22177 <118590473+Rajesh22177@users.noreply.github.com>
2024-09-04 00:23:26 +0530

    Add files via upload

---
+++ 2Day.cpp
@@ -0,0 +1,99 @@
+//  2 Day
+
+//  First Program
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"BHARDWAJ";
+    return 0;
+}
+
+//  Variables
+ 
+ #include<iostream>
+ using namespace std;
+ int main(){
+     int age=4;
+     char ch='a';
+    float f=33.3f;
+     bool frr=true;
+     double price=100.99;
+     return 0;
+ }
+ 
+ 
+//  TYPE CASTING
+
+#include<iostream>
+using namespace std;
+int main(){
+    char grade='A';
+    int value=grade;
+    cout<<value<<endl;
+    return 0;
+}
+
+
+//  INput in c++
+
+cin>>data;
+
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    cout<<"your age is"<<age<<endl;
+    return 0;
+    
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    double price;
+    cout<<"Enter the price: ";
+    cin>>price;
+    cout<<"you enterd price="<<price<<endl;
+    return 0;
+    
+}
+
+
+// Operators
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10, b=5;
+    cout<<"The result is"<<endl;
+    cout<<"the sum of a and b is"<<(a+b)<<endl;
+    cout<<"The subtraction of a and b is"<<(a-b)<<endl;
+    cout<<"The multiplication of a and b is"<<(a*b)<<endl;
+    cout<<"the division of a and b is"<<(a/b)<<endl;
+    return 0;
+}
+
+Arithmetic
+Relational
+Logical
+botwise
+
+// sum of two number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b, sum;
+    cout<<"Enter the two number"<<endl;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"the sum of a and b is  "<<sum;
+    return 0;
+    
+}
+
+// Unary operators
+
+// Increment ++
+// Decrement --
